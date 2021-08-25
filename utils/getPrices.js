c49b1941031581ad5cb7c1eb74a309b9dc432098
Guilherme <gui.andra142@gmail.com>
2021-08-25 01:01:23 -0300

    alterações

--- utils/getPrices.js
+++ utils/getPrices.js
@@ -6,14 +6,14 @@ module.exports = {
     getCoinGeckoPrice: async (id, decimals) => { 
         let res = await coinGeckoClient.coins.fetch(id).catch(err => { console.log(err) })
 
-        let formatter = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'USD', maximumFractionDigits: decimals })
+        let formatterCurrency = Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'USD', maximumFractionDigits: decimals })
 
         let price = res.data.market_data.current_price.usd
         let price_change_percentage_24h = res.data.market_data.price_change_percentage_24h
     
         return {
-            price: formatter.format(price),
-            price_change_percentage_24h: formatter.format(price_change_percentage_24h)
+            price: formatterCurrency.format(price),
+            price_change_percentage_24h: parseFloat(price_change_percentage_24h).toFixed(2)
         }
     },
 
